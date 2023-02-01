89cb8d1c8c973edd5017b1b1f59c82a372193fea
Luke <luke@lightscale.io>
2023-02-01 13:28:26 +0900

    feat(prover_lib, l2client): create prover_lib, l2client library
    - Previously, proof creating process was all written on /bin/src/prover.rs
    - prover_lib is created for easier implementation of grpc server taking
      request to create proof
    - l2client is created for easier implementation for json-rpc client usage from prover_lib

---
+++ l2client/src/lib.rs
@@ -0,0 +1,44 @@
+use anyhow::Result;
+use jsonrpsee::{
+    core::client::ClientT,
+    http_client::{HttpClient, HttpClientBuilder},
+    rpc_params,
+};
+use once_cell::sync::Lazy;
+use types::eth::BlockResult;
+use zkevm::utils::read_env_var;
+
+pub static RPC_URL: Lazy<String> =
+    Lazy::new(|| read_env_var("RPC_URL", "http://localhost:8545".to_string()));
+
+pub struct L2Client {
+    http_client: HttpClient,
+}
+
+impl Default for L2Client {
+    fn default() -> Self {
+        Self {
+            http_client: HttpClientBuilder::default().build(RPC_URL.clone()).unwrap(),
+        }
+    }
+}
+
+impl L2Client {
+    pub fn new(url: String) -> Self {
+        Self {
+            http_client: HttpClientBuilder::default().build(url.clone()).unwrap(),
+        }
+    }
+
+    pub async fn get_trace_by_block_number_hex(
+        &self,
+        block_number_hex: String,
+    ) -> Result<BlockResult> {
+        let params = rpc_params![block_number_hex.clone()];
+        let trace_result = self
+            .http_client
+            .request("kanvas_getBlockResultByNumberOrHash", params)
+            .await;
+        Ok(trace_result.unwrap())
+    }
+}
---
+++ prover_lib/src/args.rs
@@ -0,0 +1,27 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    /// Get params and write into file.
+    #[clap(short, long = "params")]
+    pub params_path: Option<String>,
+    /// Get seed and write into file.
+    #[clap(long = "seed")]
+    pub seed_path: Option<String>,
+    /// Get BlockTrace from file or dir.
+    #[clap(short, long = "trace")]
+    pub race_path: Option<String>,
+    /// Option means if generates evm proof.
+    /// Boolean means if output evm proof.
+    #[clap(long = "evm")]
+    pub evm_proof: Option<bool>,
+    /// Option means if generates state proof.
+    /// Boolean means if output state proof.
+    #[clap(long = "state")]
+    pub state_proof: Option<bool>,
+    /// Option means if generates agg proof.
+    /// Boolean means if output agg proof.
+    #[clap(long = "agg")]
+    pub agg_proof: Option<bool>,
+}
