4b0f64fbcaed94855007181e093cd7bde9702ee8
dongchangYoo <yoodc8167@gmail.com>
2023-06-19 19:27:41 +0900

    feat(prover-grpc): basic grpc server impl. and mockup grpc client
    - grpc-sever initiated with config.json
    - grpc-server run with prover_service(prover_lib)
    - supporting grpc api: prove(), spec()
    - integration test pass (via cmd-line)

--- prover-grpc/src/prover_server.rs
+++ prover-grpc/src/prover_server.rs
@@ -1,11 +1,15 @@
 pub mod l2_client;
 pub mod prover_lib;
+pub mod server;
 pub mod utils;
 pub mod proof {
     tonic::include_proto!("proof");
 }
+
 use anyhow::Result;
 use clap::Parser;
+use server::GrpcServer;
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -18,6 +22,11 @@ struct Args {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok();
     env_logger::init();
+
+    let args = Args::parse();
+    let config_path = PathBuf::from(&args.config_path.unwrap());
+    let mut server = GrpcServer::from_config_file(&config_path);
+    server.start().await?;
 
     Ok(())
 }
